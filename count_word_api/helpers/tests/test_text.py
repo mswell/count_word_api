89ec6080318e02ff8b02463933b3c7981e38f96e
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-12-14 22:01:40 -0200

    Add helper to remove punctuation from text

--- count_word_api/helpers/tests/test_text.py
+++ count_word_api/helpers/tests/test_text.py
@@ -1,4 +1,4 @@
-from count_word_api.helpers.text import html_to_text
+from count_word_api.helpers.text import html_to_text, remove_punctuation
 
 
 class TestTextHelper(object):
@@ -11,3 +11,12 @@ class TestTextHelper(object):
         expected_text = 'RAFAEL TESTE TESTE2 TESTE3'
         text = html_to_text(self.myhtml)
         assert expected_text == text
+
+    def test_remove_punctuation(self):
+        expected_text = "Rafael"
+        punctuation = "!@#$%^&*()[]{}';:,./<>?\|`~-=_+\""
+        for char in punctuation:
+            text = "Rafael"
+            text += char
+            text = remove_punctuation(text)
+            assert expected_text == text
--- count_word_api/helpers/text.py
+++ count_word_api/helpers/text.py
@@ -6,3 +6,9 @@ def html_to_text(html_text):
     soup = BeautifulSoup(html_text, "html.parser")
     clean_html = ' '.join(soup.find_all(text=True))
     return clean_html
+
+
+def remove_punctuation(text):
+    translate_table = {ord(c): ""
+                       for c in "!@#$%^&*()[]{}';:,./<>?\|`~-=_+\""}
+    return text.translate(translate_table)
